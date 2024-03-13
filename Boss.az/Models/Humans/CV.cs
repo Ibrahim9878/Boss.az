135f16b951506e27388ebbcc4fd9d1a713b973a3
Talib_ip88 <Talib_ip88@STHQ012E-15>
2024-03-13 20:34:43 +0400

    Add project files.

---
+++ Boss.az/Models/Humans/CV.cs
@@ -0,0 +1,82 @@
+﻿namespace Boss.az.Models.Humans;
+
+public class CV
+{
+	private string _profession;
+	public string Profession
+	{
+		get { return _profession; }
+		set { if (value.Length < 3 && value.Length > 19) throw new Exception("profession length must [3;19]"); _profession = value; }
+	}
+	private string _school;
+
+	public string School
+	{
+		get { return _school; }
+		set { if (value.Length < 3 && value.Length > 15) throw new Exception("school length must [3;15]"); _profession = value; }
+	}
+
+	private string _skills;
+
+	public string Skills
+	{
+		get { return _skills; }
+		set { if (value.Length < 3 && value.Length > 19) throw new Exception("profession length must [3;19]"); _skills = value; }
+	}
+	public Dictionary<char,int> LevelOfLanguage { get; set; }
+
+
+	private List<string> Companies { get; set; }
+	public DateTime StartDateTime { get; set; }
+	public DateTime EndDateTime { get; set; }
+
+	private int _languageCount;
+
+	public int LanguageCount
+	{
+		get { return _languageCount; }
+		set { if (value < 0) throw new Exception("Language cant be negative"); _languageCount = value; }
+	}
+	public bool HasCertification { get; set; }
+
+    public CV(string profession,  string school, string skills, List<string> companies, int languageCount, bool hasCertification, DateTime date1, DateTime date2, Dictionary<char,int> levelOfLanguage)
+    {
+        _profession = profession;
+        _school = school;
+        _skills = skills;
+        Companies = companies;
+        _languageCount = languageCount;
+        HasCertification = hasCertification; 
+        StartDateTime = date1;
+        EndDateTime = date2;
+        LevelOfLanguage = levelOfLanguage;
+    }
+    public CV()
+    {
+        Companies = new List<string>();
+        _profession = string.Empty;
+        _school = string.Empty;
+        _skills = string.Empty;
+        _languageCount = 0;
+        HasCertification = false;
+		LevelOfLanguage = new();
+    }
+    public override string ToString()
+    {
+		string message = "";
+		foreach (var item in Companies)
+		{
+			message += item;
+			message += ", ";
+		}
+		string text = "";
+		foreach (var item in LevelOfLanguage)
+		{
+			text += item.Key+" " + item.Value +"\n";
+
+		}
+        return $"{Profession} {School} {Skills} {StartDateTime} {EndDateTime} {LanguageCount} {HasCertification} {message} {text}" ;
+    }
+
+}
+
---
+++ Boss.az/Models/Humans/Worker.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Xml.Linq;
+namespace Boss.az.Models.Humans;
+
+public class Worker
+{
+    public static int id = 0;
+    private string _name;
+
+	public string Name
+	{
+		get { return _name; }
+		set { if (value.Length < 3 && value.Length > 19) throw new Exception("name length must [3;19]"); _name = value; }
+	}
+
+    private string _surname;
+
+    public string Surname
+    {
+        get { return _surname; }
+        set { if (value.Length < 3 && value.Length > 19) throw new Exception("surname length must [3;19]"); _surname = value; }
+    }
+
+    private string _sheher;
+
+    public string Sheher
+    {
+        get { return _sheher; }
+        set { if (value.Length < 3 && value.Length > 19) throw new Exception("Sheher length must [3;19]"); _sheher = value; }
+    }
+
+    private string _phone;
+
+    public string Phone
+    {
+        get { return _phone; }
+        set { if (!(value.Length == 10)) throw new Exception("Phone must be 10 characters"); _phone = value; }
+    }
+    private int _age;
+
+    public int Age
+    {
+        get { return _age; }
+        set { if (_age <= 18 && _age >= 55) throw new Exception("Age must [18;55]"); else _age = value; }
+    }
+    public CV cv{ get; set; }
+
+    public Worker(string name, string surname, string sheher, string phone, int age, CV cv):this()
+    {
+        Name = name;
+        Surname = surname;
+        Sheher = sheher;
+        Phone = phone;
+        Age = age;
+        this.cv = cv;
+    }
+
+    public Worker()
+    {
+        id++;
+        _age = 0;
+        _name = string.Empty;
+        _sheher = string.Empty;
+        _phone = string.Empty;
+        _surname = string.Empty;
+        this.cv = new CV();
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {Surname} {Age} {Sheher} {Phone}" + cv.ToString();
+    }
+}
